027f6a28023bcb9a6c92a95bbcdc2913208fb093
Michael COULLERET <michael@coulleret.pro>
2017-01-22 17:34:48 +0100

    [develop] Init project

---
+++ web/app.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the mc-framework project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
